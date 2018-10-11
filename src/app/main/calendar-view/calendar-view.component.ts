1c8204029f7801ed206d301d6c9feff7d924be7c
julia galiulina <julie_gal@mail.ru>
2018-10-11 17:19:45 +0300

    use scss as default style sheet

--- src/app/main/calendar-view/calendar-view.component.ts
+++ src/app/main/calendar-view/calendar-view.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calendar-view',
   templateUrl: './calendar-view.component.html',
-  styleUrls: ['./calendar-view.component.css']
+  styleUrls: ['./calendar-view.component.scss']
 })
 export class CalendarViewComponent implements OnInit {
 
