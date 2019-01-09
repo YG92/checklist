8475d100dd605c69e16c20c5c485459af2c650b0
julia galiulina <92juliegal@gmail.com>
2019-01-09 11:24:21 +0300

    added fixture file

--- src/app/main/services/storage-service/storage.service.ts
+++ src/app/main/services/storage-service/storage.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Task } from '../../task';
+import { fixtureTasks } from '../../fixture';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
 
-  constructor() { }
+  constructor() {
+    this.initTasks();
+  }
+
+  initTasks(): void {
+    const tasks = this.getTasks();
+    if (!tasks) {
+      this.setTasks(fixtureTasks);
+    }
+  }
 
   getTasks(): Task[] {
-    return JSON.parse(localStorage.getItem('tasks')) || [];
+    return JSON.parse(localStorage.getItem('tasks'));
   }
 
   setTasks(tasks): void {
